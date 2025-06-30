f8edd6b62db6de595e9596079033d0fa545e06a1
krina <solankikrina704@gmail.com>
2025-06-30 17:20:32 +0530

    [ADD]

---
+++ source_clickable_cr/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo Module Developed by Candidroot Solutions Pvt. Ltd.
+# See LICENSE file for full copyright and licensing details.
+from . import clickable_picking
+from . import clickable_invoices
